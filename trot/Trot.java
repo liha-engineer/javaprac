e92b9c2b71e675af72a0f3fbb7b22ee9430e9753
liha_engineer <122168823+liha-engineer@users.noreply.github.com>
2023-01-09 01:12:02 +0900

    feat: {Trot.java} 추가
    배열, for문, index

---
+++ trot/Trot.java
@@ -0,0 +1,11 @@
+package trot;
+public class Trot {
+    public static void main(String[] args) {
+
+        String[] trot = {"영탁", "찬원", "영웅", "민호", "동원"};
+        for (int i = 0; i < trot.length ; i++) {
+            System.out.println(trot[i] + "의 인덱스 번호는 " + i);
+
+        }
+    }
+}
